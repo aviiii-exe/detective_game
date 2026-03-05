c6f1913b99064959951f5468ee9ec429ad618d37
shreyasNaik0101 <shreyas.naik0805@gmail.com>
2026-03-05 16:32:26 +0530

    Initialised backend server setup

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const caseRoutes = require("./routes/caseRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", caseRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
